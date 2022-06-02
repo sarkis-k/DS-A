d97e49433d5fa97c71f04ed7a8dab8b7d917c5db
sargis24 <sargis.leva@gmail.com>
2022-06-01 17:39:46 -0700

    LinkedList implementation complete and tested

---
+++ linked_list/main.py
@@ -0,0 +1,29 @@
+from singly import LinkedList
+
+llist = LinkedList()
+
+print("size: ", len(llist))
+llist.push("something")
+llist.push("something")
+print("size: ", len(llist))
+print(llist)
+llist.push("something""something""something")
+llist.push("something""something")
+llist.push("something""something")
+llist.push("something""something")
+print("List content: ", llist)
+print("popping...")
+llist.pop()
+print("List content: ", llist)
+print("Does list contain ")
+if llist.contains("something""something"):
+    print("Yes")
+else:
+    print("No")
+print("Deleting ")
+llist.delete("something""something")
+print("List content: ", llist)
+print("Pushing another onto end.")
+llist.append("something")
+
+print("List content: ", llist)
--- linked_list/singly.py
+++ linked_list/singly.py
@@ -1,5 +1,6 @@
 from node import Node
 
+
 class LinkedList(object):
     def __init__(self):
         self.head = None
@@ -14,6 +15,7 @@ class LinkedList(object):
             count += 1
             current = current.get_next()
         return count
+
     def __str__(self):
         current = self.head
         output = ""
@@ -21,3 +23,47 @@ class LinkedList(object):
             output += str(current) + " -> "
             current = current.get_next()
         return output
+
+    def pop(self):
+        if self.head:
+            self.head = self.head.get_next()
+        else:
+            raise IndexError("Unable to pop from empty list")
+
+    def contains(self, value):
+        found = False
+        current = self.head
+        while current and not found:
+            if current.get_data() == value:
+                found = True
+            else:
+                current = current.get_next()
+        return found
+
+    def delete(self, value):
+        current = self.head
+        prev = None
+        while current:
+            if current.get_data() == value:
+                if prev:
+                    prev.set_next(current.get_next())
+                else:
+                    self.head = current.get_next()
+            else:
+                prev = current
+            current = current.get_next()
+
+    def push(self, value):
+        node = Node(value)
+        node.set_next(self.head)
+        self.set_head(node)
+
+    def append(self, value):
+        node = Node(value)
+        current = self.head
+        if not current:
+            self.head = node
+            return
+        while current.get_next():
+            current = current.get_next()
+        current.set_next(node)
