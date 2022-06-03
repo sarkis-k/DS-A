289619e04d788261892f3548a204e714cfbf41d3
Sarkis Karapetyan <sargis.leva@gmail.com>
2022-06-02 19:36:07 -0700

    remove from head implemented

--- linked_list/doubly_linked.py
+++ linked_list/doubly_linked.py
@@ -7,5 +7,15 @@ class DoublyLinkedList(LinkedList):
         super().__init__()
         self.tail = None
 
+    def remove_head(self):
+        if self.head:
+            temp = self.head
+            temp.next.prev = None
+            self.head = temp.next
+            temp.next = None
+            return temp.data
+        else:
+            raise IndexError("Unable to pop from empty list")
 
 
+    def 
