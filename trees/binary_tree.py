d16c53021c1a7a897051a2608f52dde79088f08e
Sarkis Karapetyan <sargis.leva@gmail.com>
2022-06-08 19:46:23 -0700

    binary tree initial commit

---
+++ trees/binary_tree.py
@@ -0,0 +1,15 @@
+class Node:
+    def __init__(self, data) -> None:
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def get_left(self):
+        return self.left
+
+    def get_right(self):
+        return self.right
+
+    def get_value(self):
+        return self.data
+
