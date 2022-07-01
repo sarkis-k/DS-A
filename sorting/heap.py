c8c0febee2b4d5a35b6abe3cc3f18a54a2f83329
Sarkis Karapetyan <sargis.leva@gmail.com>
2022-07-01 12:18:19 -0700

    heap sort implemented

---
+++ sorting/heap.py
@@ -0,0 +1,32 @@
+def heapify(arr, n, i):
+    largest = i
+    l = 2 * i + 1
+    r = 2 * i + 2
+
+    if l < n and arr[i] < arr[l]:
+        largest = l
+
+    if r < n and arr[largest] < arr[r]:
+        largest = r
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+
+
+def heap_sort_a(arr):
+    n = len(arr)
+
+    for i in range(n // 2, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+
+
+to_sort = [3, 2, 5, 26, 46, 675, 234, 1]
+print("before sort ", to_sort)
+
+heap_sort_a(to_sort)
+print("after sort accenting", to_sort)
