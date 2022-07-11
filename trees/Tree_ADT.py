e0b82e01ef1695a91f47e9748d20775332bedbe0
Sarkis Karapetyan <sargis.leva@gmail.com>
2022-07-11 14:50:30 -0700

    Linked based binary tree implemented using ADT of tree with inherited binary tree ADT

---
+++ trees/Tree_ADT.py
@@ -0,0 +1,130 @@
+from linked_queue import LinkedQueue
+
+
+class Tree:
+    """Abstract base class representing a tree structure."""
+
+    # ------------------------------- nested Position class -------------------------------
+    class Position:
+        """An abstraction representing the location of a single element."""
+
+        def element(self):
+            """Return the element stored at this Position."""
+            raise NotImplementedError("must be implemented by subclass")
+
+        def eq(self, other):
+            """Return True if other Position represents the same location."""
+            raise NotImplementedError("must be implemented by subclass")
+
+        def ne(self, other):
+            """Return True if other does not represent the same location."""
+            return not (self == other)  # opposite of eq
+
+    # ---------- abstract methods that concrete subclass must support ----------
+    def root(self):
+        """Return Position representing the tree s root (or None if empty)."""
+        raise NotImplementedError("must be implemented by subclass ")
+
+    def parent(self, p):
+        """Return Position representing p s parent (or None if p is root)."""
+        raise NotImplementedError("must be implemented by subclass")
+
+    def num_children(self, p):
+        """Return the number of children that Position p has."""
+        raise NotImplementedError("must be implemented by subclass")
+
+    def children(self, p):
+        """Generate an iteration of Positions representing p s children."""
+        raise NotImplementedError("must be implemented by subclass")
+
+    def len(self):
+        """Return the total number of elements in the tree."""
+        raise NotImplementedError("must be implemented by subclass")
+
+    # ---------- concrete methods implemented in this class ----------
+    def is_root(self, p):
+        """Return True if Position p represents the root of the tree."""
+        return self.root() == p
+
+    def is_leaf(self, p):
+        """Return True if Position p does not have any children."""
+        return self.num_children(p) == 0
+
+    def is_empty(self):
+        """Return True if the tree is empty."""
+
+    def depth(self, p):
+        """Return the number of levels separating Position p from the root."""
+        if self.is_root(p):
+            return 0
+        else:
+            return 1 + self.depth(self.parent(p))
+
+    def _height2(self, p):  # time is linear in size of subtree
+        """Return the height of the subtree rooted at Position p."""
+        if self.is_leaf(p):
+            return 0
+        else:
+            return 1 + max(self._height2(c) for c in self.children(p))
+
+    def height(self, p=None):
+        """Return the height of the subtree rooted at Position p.
+
+        If p is None, return the height of the entire tree.
+        """
+        if p is None:
+            p = self.root()
+        return self._height2(p)  # start _height2 recursion
+
+    def __iter__(self):
+        """Generate an iteration of the tree's elements."""
+        for p in self.positions():
+            yield p.element()
+
+    def positions(self):
+        """Generate an iteration of the tree's positions."""
+        return self.preorder()
+
+    def preorder(self):
+        """Generate a preorder iteration os positions in the tree."""
+        if not self.is_empty():
+            for p in self._subtree_preorder(self.root()):
+                yield p
+
+    def _subtree_preorder(self, p):
+        """Generate a preorder iteration of positions in subtree rooted as p."""
+        yield p
+        for c in self.children(p):
+            for other in self._subtree_preorder(c):
+                yield other
+
+    def preorder_indent(T, p, d):
+        """Print preorder representation of subtree T rooted at p at depth d."""
+        print(2*d*" " + str(p.element()))
+        for c in T.children(p):
+            preorder_indent(T, c, d+1)
+
+
+    def postorder(self):
+        """Generate a postorder iteration os positions in the tree."""
+        if not self.is_empty():
+            for p in self._subtree_postorder(self.root()):
+                yield p
+
+    def _subtree_postorder(self, p):
+        """Generate a postorder iteration of positions in subtree rooted as p."""
+        for c in self.children(p):
+            for other in self._subtree_preorder(c):
+                yield other
+        yield p
+
+    def breadthfirst(self):
+        """Generate a breadth-first iteration of the positions of the tree."""
+        if not self.is_empty():
+            fringe = LinkedQueue()
+            fringe.enqueue(self.root())
+            while not fringe.is_empty():
+                p = fringe.dequeue()
+                yield p
+                for c in self.children(p):
+                    fringe.enqueue(c)
